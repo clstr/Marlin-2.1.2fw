b29f7721bc1fbd4c51d787da67364bcc30b83f1f
Sergio Palomino <1195526+clstr@users.noreply.github.com>
2023-04-21 11:34:25 -0400

    slower homing and z probing feedrates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1495,7 +1495,7 @@
 #define XY_PROBE_FEEDRATE (140*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (9*60)
+#define Z_PROBE_FEEDRATE_FAST (8*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
@@ -2100,7 +2100,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (140*60), (140*60), (10*60) }
+#define HOMING_FEEDRATE_MM_M { (130*60), (130*60), (10*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
