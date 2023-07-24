b59a546dcf6e27b4684e0e8279d1a5425908e403
clstr <1195526+clstr@users.noreply.github.com>
2023-07-23 21:36:10 -0400

    faster probe feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1491,7 +1491,7 @@
 #define PROBING_MARGIN 45
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (140*60)
+#define XY_PROBE_FEEDRATE (180*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (7*60)
