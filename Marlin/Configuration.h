f9e087ee0176959aea96916e212c99e4138c4681
Sergio Palomino <1195526+clstr@users.noreply.github.com>
2023-08-28 10:15:13 -0400

    Enable Input Shaping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "uwu daddy ._."
+#define CUSTOM_MACHINE_NAME "uwu"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
